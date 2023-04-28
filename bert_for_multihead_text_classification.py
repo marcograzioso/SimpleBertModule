0337e9bd5584a70b08a599fdc9a9bdb6c7a861ba
Marco Grazioso <mar.grazioso@gmail.com>
2023-04-28 16:42:10 +0100

    adding support for multihead classification

--- bert_for_multihead_text_classification.py
+++ bert_for_multihead_text_classification.py
@@ -13,13 +13,13 @@ from bert_utils import *
 from tqdm import tqdm
 
 
-class Classifier(nn.Module):
+class MultiHeadClassifier(nn.Module):
     """
     Class which implements an exlcusive multiclass classifier 
     """
     def __init__(self, model_name, params = None):
-        super(Classifier, self).__init__()
-        # params = params
+        super(MultiHeadClassifier, self).__init__()
+        self.params = params
         # Load the BERT-based encoder
         self.encoder = AutoModel.from_pretrained(model_name, cache_dir="new_cache_dir/")
         self.tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir="new_cache_dir/")
@@ -30,19 +30,24 @@ class Classifier(nn.Module):
         self.cls_size = int(config.hidden_size)
         # Dropout is applied before the final classifier
         self.input_dropout = nn.Dropout(p=params.out_dropout_rate)
-        # Final linear classifier
-        self.fully_connected_layer = nn.Linear(self.cls_size,params.num_labels)
+        # Final linear classifiers
+        heads = []
+        for i in range(params.num_heads):
+            heads.append(nn.Linear(self.cls_size,params.num_labels[i]))
+        self.fully_connected_layers = nn.ModuleList(heads)
 
     def forward(self, input_ids, attention_mask):
         # encode all outputs
         model_outputs = self.encoder(input_ids, attention_mask)
-        # just select the vector associated to the [CLS] symbol used as
+        # select the vector associated to the [CLS] symbol used as
         # first token for ALL sentences
         encoded_cls = model_outputs.last_hidden_state[:,0]
         # apply dropout
         encoded_cls_dp = self.input_dropout(encoded_cls)
         # apply the linear classifier
-        logits = self.fully_connected_layer(encoded_cls_dp)
+        logits = []
+        for fully_connected_layer in self.fully_connected_layers:
+            logits.append(fully_connected_layer(encoded_cls_dp))
         # return the logits
         return logits, encoded_cls
     
@@ -61,8 +66,8 @@ class Classifier(nn.Module):
             num_warmup_steps = int(num_train_steps * params.warmup_proportion)
             # Initialize the scheduler
             params.scheduler = get_constant_schedule_with_warmup(params.optimizer, num_warmup_steps = num_warmup_steps)
-        train_dataloader = generate_data_loader(train_examples, params.label_to_id, self.tokenizer, params.max_seq_length, params.batch_size, do_shuffle = True)
-        dev_dataloader = generate_data_loader(dev_examples, params.label_to_id, self.tokenizer, params.max_seq_length, params.batch_size, do_shuffle = True)
+        train_dataloader = generate_data_loader_for_multihead(train_examples, params.label_to_id, self.tokenizer, params.max_seq_length, params.batch_size, do_shuffle = True)
+        dev_dataloader = generate_data_loader_for_multihead(dev_examples, params.label_to_id, self.tokenizer, params.max_seq_length, params.batch_size, do_shuffle = True)
         start_training(self, params, train_dataloader, dev_dataloader)
 
 
@@ -101,6 +106,7 @@ def start_training(model, params, train_dataloader, dev_dataloader):
 
         # For each batch of training data...
         for step, batch in enumerate(tqdm(train_dataloader)):
+            loss = 0
             if step % params.print_each_n_step == 0 and not step == 0:
                 # Calculate elapsed time in minutes.
                 elapsed = format_time(time.time() - t0)
@@ -117,7 +123,8 @@ def start_training(model, params, train_dataloader, dev_dataloader):
             # forward pass: compute predicted outputs by passing inputs to the model
             train_logits, _ = model(b_input_ids, b_input_mask)
             # calculate the loss        
-            loss = params.nll_loss(train_logits, b_labels)      
+            for i in range(params.num_heads):
+                loss += params.nll_loss(train_logits[i], b_labels[:,i])      
             # backward pass: compute gradient of the loss with respect to model parameters
             loss.backward() 
             # perform a single optimization step (parameter update)
@@ -158,11 +165,11 @@ def start_training(model, params, train_dataloader, dev_dataloader):
 
         # Measure how long the validation run took.
         test_time = format_time(time.time() - t0)
-
-        print("  Accuracy: {0:.3f}".format(dev_accuracy))
-        print("  F1 weighted: {0:.3f}".format(dev_f1_weighted))
-        print("  Test Loss: {0:.3f}".format(avg_dev_loss))
-        print("  Test took: {:}".format(test_time))
+        for i_h in range(params.num_heads):
+            print("  Accuracy head {}: {:.3f}".format(i_h,dev_accuracy[i_h]))
+            print("  F1 weightedhead {}: {:.3f}".format(i_h,dev_f1_weighted[i_h]))
+        print("  Test Loss: {:.3f}".format(avg_dev_loss))
+        print("  Test took: {}".format(test_time))
 
         # Record all statistics from this epoch.
         training_stats.append(
@@ -177,11 +184,12 @@ def start_training(model, params, train_dataloader, dev_dataloader):
         )
 
         # Save the model if the performance on the development set increases
-        if dev_accuracy > best_dev_accuracy:
-            best_dev_accuracy = dev_accuracy
+        mean_accuracy = sum(dev_accuracy)/params.num_heads
+        if mean_accuracy > best_dev_accuracy:
+            best_dev_accuracy = mean_accuracy
             torch.save(model, params.output_model_name)
             print("\n  Saving the model during epoch " + str(epoch_i + 1))
-            print("  Actual Best Validation Accuracy: {0:.3f}".format(best_dev_accuracy))
+            print("  Actual Best Validation Accuracy: {:.3f} - from these accuracies: {}".format(mean_accuracy, dev_accuracy))
 
 
 
@@ -198,8 +206,11 @@ def evaluate(dataloader, model, params, print_classification_output=False, epoch
     print_classification_output: to log the classification outcomes 
     ''' 
     total_loss = 0
-    gold_classes = [] 
-    system_classes = []
+    gold_classes = [[] for i in range(params.num_heads)] 
+    system_classes = [[] for i in range(params.num_heads)]
+    accuracy = [None] * params.num_heads
+    preds = [None] * params.num_heads
+    output = [None] * params.num_heads
     tokenizer = model.tokenizer
     if print_classification_output:
         print("\n------------------------")
@@ -218,65 +229,71 @@ def evaluate(dataloader, model, params, print_classification_output=False, epoch
         # Tell pytorch not to bother with constructing the compute graph during
         # the forward pass, since this is only needed for backprop (training).
         with torch.no_grad():
+            loss = 0
             # Each batch is classifed        
             logits, _ = model(b_input_ids, b_input_mask)
             # Evaluate the loss. 
-            total_loss += params.nll_loss(logits, b_labels)
+            for i in range(params.num_heads):
+                loss += params.nll_loss(logits[i], b_labels[:,i])
+            total_loss += loss
 
         # Accumulate the predictions and the input labels
-        _, preds = torch.max(logits, 1)
-        system_classes += preds.detach().cpu()
-        gold_classes += b_labels.detach().cpu()
+        for i in range(params.num_heads):
+            _, preds[i] = torch.max(logits[i], 1)
+            system_classes[i] += preds[i].detach().cpu()
+            gold_classes[i] += b_labels[:,i].detach().cpu()
 
         # Print the output of the classification for each input element
         if print_classification_output:
             for ex_id in range(len(b_input_mask)):
                 input_strings = tokenizer.decode(b_input_ids[ex_id], skip_special_tokens=True)
                 # convert class id to the real label
-                predicted_label = params.id_to_label[preds[ex_id].item()]
-                gold_standard_label = "UNKNOWN"
-                # convert the gold standard class ID into a real label
-                if b_labels[ex_id].item() in params.id_to_label:
-                    gold_standard_label = params.id_to_label[b_labels[ex_id].item()]
-                # put the prefix "[OK]" if the classification is correct
-                output = '[OK]' if predicted_label == gold_standard_label else '[NO]'
+                for i in range(params.num_heads):
+                    predicted_label = params.id_to_label[preds[i][ex_id].item()]
+                    gold_standard_label = "UNKNOWN"
+                    # convert the gold standard class ID into a real label
+                    if b_labels[ex_id].item() in params.id_to_label:
+                        gold_standard_label = params.id_to_label[b_labels[i][ex_id].item()]
+                    # put the prefix "[OK]" if the classification is correct
+                    output[i] = '[OK]' if predicted_label[i] == gold_standard_label[i] else '[NO]'
                 # print the output
-                print(output+"\t"+gold_standard_label+"\t"+predicted_label+"\t"+input_strings)
+                print("\t".join(output) + "\t" + gold_standard_label + "\t" + predicted_label + "\t" + input_strings)
     
     # Calculate the average loss over all of the batches.
     avg_loss = total_loss / len(dataloader)
     avg_loss = avg_loss.item()
 
     # Report the final accuracy for this validation run.
-    system_classes = torch.stack(system_classes).numpy()
-    gold_classes = torch.stack(gold_classes).numpy()
-    accuracy = np.sum(system_classes == gold_classes) / len(system_classes)
+    for i_h in range(params.num_heads):
+        system_classes[i_h] = torch.stack(system_classes[i_h]).numpy()
+        gold_classes[i_h] = torch.stack(gold_classes[i_h]).numpy()
+        accuracy[i_h] = np.sum(system_classes[i_h] == gold_classes[i_h]) / len(system_classes[i_h])
 
-    if params.print_result_summary or params.log_result_summary: 
-        summary = "\n------------------------"
-        summary += "  Summary " + str(epoch)
-        summary += "------------------------\n"
-        #remove unused classes in the test material
-        filtered_label_list = []
-        for i in range(len(params.labels)):
-            if i in gold_classes:
-                filtered_label_list.append(params.id_to_label[i])
-        summary += classification_report(gold_classes, system_classes, digits=3, target_names=filtered_label_list)
+        if params.print_result_summary or params.log_result_summary: 
+            summary = "\n------------------------"
+            summary += "  Summary " + str(epoch)
+            summary += "------------------------\n"
+            #remove unused classes in the test material
+            filtered_label_list = []
+            for i in range(len(params.labels[i_h])):
+                if i in gold_classes[i_h] or i in system_classes[i_h]:
+                    filtered_label_list.append(params.id_to_label[i_h][i])
+            summary += classification_report(gold_classes[i_h], system_classes[i_h], digits=3, target_names=filtered_label_list)
 
-        summary += "\n------------------------"
-        summary += "  Confusion Matrix"
-        summary += "------------------------\n"
-        conf_mat = confusion_matrix(gold_classes, system_classes)
-        for row_id in range(len(conf_mat)):
-            summary += filtered_label_list[row_id] + "\t" +str(conf_mat[row_id]) + "\n"
-        if params.print_result_summary:
-            print(summary)
-        if params.log_result_summary:
-            with open("training_log.log", mode="a", encoding="utf-8") as f:
-                f.write(summary)
-            f.close()
+            summary += "\n------------------------"
+            summary += "  Confusion Matrix"
+            summary += "------------------------\n"
+            conf_mat = confusion_matrix(gold_classes[i_h], system_classes[i_h])
+            for row_id in range(len(conf_mat)):
+                summary += filtered_label_list[row_id] + "\t" +str(conf_mat[row_id]) + "\n"
+            if params.print_result_summary:
+                print(summary)
+            if params.log_result_summary:
+                with open("training_log.log", mode="a", encoding="utf-8") as f:
+                    f.write(summary)
+                f.close()
 
-    return avg_loss, accuracy, f1_score(gold_classes, system_classes, average='weighted')
+    return avg_loss, accuracy, [f1_score(gold_classes[i_h], system_classes[i_h], average='weighted') for i_h in range(params.num_heads)]
 
 
 def getClassProbabilities(classifier, dataloader, device):
@@ -310,6 +327,43 @@ def getClassProbabilities(classifier, dataloader, device):
                 examples.append(input_strings)
                 probs.append(predicted_prob)
     return examples, probs
+
+def getClassProbabilitiesForMultihead(classifier, dataloader, device):
+    '''
+    Propagate the data through the model and obtain the probabilities
+    ''' 
+    tokenizer = classifier.tokenizer
+    examples = []
+    total_probs = []
+    # For each batch of examples from the input dataloader
+    for batch in tqdm(dataloader, desc="passing data through the model"):  
+        
+        # Unpack this training batch from our dataloader. Notice this is populated 
+        # in the method `generate_data_loader`
+        b_input_ids = batch[0].to(device)
+        b_input_mask = batch[1].to(device)
+        # b_labels = batch[2].to(device)
+        
+        # Tell pytorch not to bother with constructing the compute graph during
+        # the forward pass, since this is only needed for backprop (training).
+        with torch.no_grad():
+            # Each batch is classifed        
+            logits, _ = classifier(b_input_ids, b_input_mask)
+            # get the prediction and use the softmax to obtain a probability
+            out = [None for x in range(classifier.params.num_heads)]
+            classes = [None for x in range(classifier.params.num_heads)]
+            for i, l in enumerate(logits):
+                out[i] = torch.nn.functional.softmax(l, dim=1)
+                classes[i] = out[i].detach().cpu()
+            for ex_id in range(len(b_input_mask)):
+                probs = [None for x in range(classifier.params.num_heads)]
+                input_strings = tokenizer.decode(b_input_ids[ex_id], skip_special_tokens=False)
+                for i, cl in enumerate(classes):
+                    predicted_prob = cl[ex_id].tolist()
+                    probs[i] = predicted_prob
+                total_probs.append(probs)
+                examples.append(input_strings)
+    return examples, total_probs
 
 
 def getSentenceEmbeddings(classifier, dataloader, params):
--- bert_utils.py
+++ bert_utils.py
@@ -10,7 +10,7 @@ class ClassifierParameters():
     """
     def __init__(self, label_list, max_seq_length = 32, out_dropout_rate = 0.1, batch_size = 32, learning_rate = 2e-5, nll_loss = torch.nn.CrossEntropyLoss(ignore_index=-1), 
         output_model_name = "best_model.pickle", num_train_epochs = 10, apply_scheduler = True, warmup_proportion= 0.1, print_each_n_step = 6,
-        log_result_summary= False, print_result_summary = False ):
+        log_result_summary= False, print_result_summary = False, num_heads = 1 ):
         
         self.print_result_summary = print_result_summary
         self.log_result_summary = log_result_summary
@@ -26,7 +26,7 @@ class ClassifierParameters():
         # If there's a GPU available...
         if torch.cuda.is_available():    
             # Tell PyTorch to use the GPU.    
-            self.device = torch.device("cuda")
+            self.device = torch.device("cuda:0")
             print('There are %d GPU(s) available.' % torch.cuda.device_count())
             print('We will use the GPU:', torch.cuda.get_device_name(0))
         # If not...
@@ -38,7 +38,8 @@ class ClassifierParameters():
         self.max_seq_length = max_seq_length
         # dropout applied to the embedding produced by BERT before the classifiation
         self.out_dropout_rate = out_dropout_rate
-
+        # number of linear heads   
+        self.num_heads = num_heads
         # --------------------------------
         # Training parameters
         # --------------------------------
@@ -69,10 +70,22 @@ class ClassifierParameters():
         # Here a `Constant schedule with warmup`can be activated. More details here
         # https://huggingface.co/transformers/main_classes/optimizer_schedules.html#transformers.get_constant_schedule_with_warmup
         self.warmup_proportion = warmup_proportion
+        if type(label_list) is list and type(label_list[0]) is not list:  
+            self.num_labels = len(label_list)
+            self.label_to_id , self.id_to_label = generate_label_maps(label_list)
+            self.labels = label_list
+        elif type(label_list) is list and type(label_list[0]) is list:
+            self.num_labels = []
+            self.label_to_id = []
+            self.id_to_label = []
+            self.labels = []
+            for l in label_list:
+                self.num_labels.append(len(l))
+                self.labels.append(l)
+                l_to_id, id_to_l = generate_label_maps(l)
+                self.label_to_id.append(l_to_id)
+                self.id_to_label.append(id_to_l)
 
-        self.num_labels = len(label_list)
-        self.label_to_id , self.id_to_label = generate_label_maps(label_list)
-        self.labels = label_list
         # --------------------------------
         # Log parameters
         # --------------------------------
@@ -81,7 +94,6 @@ class ClassifierParameters():
         self.print_each_n_step = print_each_n_step
         self.optimizer = None
         self.scheduler = None
-
 
 
 def format_time(elapsed):
@@ -141,6 +153,64 @@ def generate_data_loader(examples, label_map, tokenizer, max_seq_length, batch_s
             id = label_map[label]
         label_id_array.append(id)
         
+    # Convert to Tensor which are used in PyTorch
+    input_ids = torch.tensor(input_ids) 
+    input_mask_array = torch.tensor(input_mask_array)
+    label_id_array = torch.tensor(label_id_array, dtype=torch.long)
+
+    # Building the TensorDataset
+    dataset = TensorDataset(input_ids, input_mask_array, label_id_array)
+
+    if do_shuffle:
+        # this will shuffle examples each time a new batch is required
+        sampler = RandomSampler
+    else:
+        sampler = SequentialSampler
+
+    # Building the DataLoader
+    return DataLoader(
+                dataset,  # The training samples.
+                sampler = sampler(dataset), # the adopted sampler
+                batch_size = batch_size) # Trains with this batch size.
+
+
+def generate_data_loader_for_multihead(examples, label_map, tokenizer, max_seq_length, batch_size, do_shuffle = False):
+    '''
+    Generate a Dataloader given the input examples
+
+    examples: a list of pairs (input_text, label)
+    label_mal: a dictionary used to assign an ID to each label
+    tokenize: the tokenizer used to convert input sentences into word pieces
+    do_shuffle: a boolean parameter to shuffle input examples (usefull in training) 
+    ''' 
+    #-----------------------------------------------
+    # Generate input examples to the Transformer
+    #-----------------------------------------------
+    input_ids = []
+    input_mask_array = []
+    label_id_array = []
+
+    # Tokenization 
+    for (text, labels) in examples:
+        # tokenizer.encode_plus is a crucial method which:
+        # 1. tokenizes examples
+        # 2. trims sequences to a max_seq_length
+        # 3. applies a pad to shorter sequences
+        # 4. assigns the [CLS] special wor-piece such as the other ones (e.g., [SEP])
+        encoded_sent = tokenizer.encode_plus(text, add_special_tokens=True, max_length=max_seq_length, padding='max_length', truncation=True)
+        # convert input word pieces to IDs of the corresponding input embeddings
+        input_ids.append(encoded_sent['input_ids'])
+        # store the attention mask to avoid computations over "padded" elements
+        input_mask_array.append(encoded_sent['attention_mask'])
+
+        # converts labels to IDs
+        label_ids = []
+        for i, label in enumerate(labels):
+            id = -1
+            if label in label_map[i]:
+                id = label_map[i][label]
+            label_ids.append(id)
+        label_id_array.append(label_ids)
     # Convert to Tensor which are used in PyTorch
     input_ids = torch.tensor(input_ids) 
     input_mask_array = torch.tensor(input_mask_array)
